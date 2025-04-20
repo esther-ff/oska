c9afdce1f89d40cf05df1afd02e3bbf040a4947a
esther-ff <creepermaster000@gmail.com>
2025-04-20 20:35:30 +0200

    complete test, survived and more fixes

--- src/md/block_parser.rs
+++ src/md/block_parser.rs
@@ -41,22 +41,49 @@ pub(crate) struct Code {
 }
 
 #[derive(Debug)]
+pub(crate) struct IndentCode {
+    indents: Box<[StrRange]>,
+    id: usize,
+}
+
+#[derive(Debug)]
 pub(crate) struct CodeMeta {
     lang: Lang,
     info: Option<String>,
 }
 
 #[derive(Debug)]
-enum Lang {
+pub enum Lang {
     None,
     Rust,
     NotSupported(String),
+}
+
+impl Lang {
+    pub fn is_useless(&self) -> bool {
+        match self {
+            Self::None => true,
+            Self::NotSupported(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn recognize(name: &str) -> Lang {
+        match name {
+            "rust" => Lang::Rust,
+
+            "" => Lang::None,
+
+            unknown => Lang::NotSupported(unknown.to_string()),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub(crate) struct Heading {
     level: HeadingLevel,
     text: Option<StrRange>,
+    id: usize,
 }
 
 #[derive(Debug)]
@@ -83,8 +110,10 @@ pub enum Block {
     Blockquote(BlkQt),
     List(List),
     FencedCode(Code),
+    IndentedCode(IndentCode),
     Heading(Heading),
     StyleBreak(Break),
+    Eof,
 }
 
 impl Block {
@@ -99,8 +128,10 @@ impl Block {
             }
             Self::List(_) => todo!("str_range: list"),
             Self::FencedCode(code) => func(code.text.as_mut().expect("should be here")),
+            Self::IndentedCode(code) => panic!("calling `str_range` on indented code is pointless"),
             Self::Heading(heading) => func(heading.text.as_mut().expect("should be here")),
-            Self::StyleBreak(_) => {}
+            Self::StyleBreak(_) => panic!("calling `str_range` on a style break is pointless"),
+            Self::Eof => unimplemented!(),
         }
     }
 
@@ -146,18 +177,50 @@ impl Block {
     }
 
     #[inline]
-    pub fn make_heading(range: impl Into<Option<StrRange>>, level: u8) -> Block {
+    pub fn make_indented_code<T: Into<Box<[StrRange]>>>(indents: T, id: usize) -> Block {
+        Block::IndentedCode(IndentCode {
+            indents: indents.into(),
+            id,
+        })
+    }
+
+    #[inline]
+    pub fn make_heading(range: impl Into<Option<StrRange>>, level: u8, id: usize) -> Block {
         let heading_level = HeadingLevel::new(level);
 
         Block::Heading(Heading {
             level: heading_level,
             text: range.into(),
+            id,
         })
     }
 
     #[inline]
     pub fn make_style_break(id: usize) -> Block {
         Block::StyleBreak(Break { id })
+    }
+
+    fn test(self, data: &[u8]) {
+        match self {
+            Self::Paragraph(p) => {
+                p.text.map(|x| dbg!(x.resolve(data)));
+            }
+
+            Self::Heading(p) => {
+                p.text.map(|x| dbg!(x.resolve(data)));
+            }
+            Self::FencedCode(p) => {
+                p.text.map(|x| dbg!(x.resolve(data)));
+            }
+            Self::IndentedCode(p) => {
+                p.indents.into_iter().for_each(|x| {
+                    dbg!(x.resolve(data));
+                });
+            }
+            Self::Blockquote(p) => p.text.map(|x| dbg!(x.test(data))).unwrap_or(()),
+
+            _ => {}
+        };
     }
 }
 
@@ -182,10 +245,13 @@ impl BlockParser {
         id
     }
 
-    pub fn block(&mut self, walker: &mut Walker<'_>) -> Option<Block> {
+    pub fn block(&mut self, walker: &mut Walker<'_>) -> Block {
         let char = match walker.next() {
             Some(c) => c,
-            None => return None,
+            None => {
+                println!("NO MERE!");
+                return Block::Eof;
+            }
         };
 
         let pred = |x: u8| (x == ASTERISK) | (x == LINE) | (x == UNDERSCORE);
@@ -193,67 +259,81 @@ impl BlockParser {
         if pred(char) {
             match self.style_break(walker) {
                 None => {}
-                Some(sb) => return Some(sb),
+                Some(sb) => return sb,
             }
         }
 
         let block = match char {
             // Heading
-            HASH => self.heading(walker),
+            HASH => match self.heading(walker) {
+                None => self.paragraph(walker),
+                Some(val) => val,
+            },
 
             // Blockquote
             GREATER_THAN => self.blockquote(walker),
-
             // Fenced code
-            BACKTICK => self.code::<BACKTICK>(walker),
+            BACKTICK => self.fenced_code::<BACKTICK>(walker),
 
             // Fenced code again!
-            TILDE => self.code::<TILDE>(walker),
+            TILDE => self.fenced_code::<TILDE>(walker),
+
+            // Indented code
+            SPACE => match self.indented_code(walker) {
+                None => {
+                    walker.retreat(1);
+                    self.paragraph(walker)
+                }
+
+                Some(block) => return block,
+            },
 
             _ => self.paragraph(walker),
         };
 
-        Some(block)
+        block
     }
 
     pub fn paragraph(&mut self, walker: &mut Walker<'_>) -> Block {
         let initial = walker.position();
+
+        dbg!(walker.data().get(initial));
 
         while let Some(char) = walker.next() {
             match char {
                 ch if (ch == NEWLINE) && walker.is_next_char(NEWLINE) => break,
 
                 NEWLINE if walker.is_next_char(EQUALS) => {
-                    let mut heading = true;
-                    let pos = walker.position();
-                    while let Some(char) = walker.next() {
-                        match char {
-                            NEWLINE => break,
-
-                            EQUALS => {}
-
-                            _ => {
-                                heading = false;
-                                break;
-                            }
-                        }
+                    match self.handle_special_heading(walker, initial) {
+                        Some(block) => return block,
+                        None => {}
                     }
+                }
 
-                    if heading {
-                        return Self::special_heading(initial - 1, pos - 1);
+                BACKTICK => {
+                    // let pos = walker.position();
+                    let amnt_of_backticks = walker.till_not(BACKTICK);
+
+                    if amnt_of_backticks >= 2 {
+                        walker.retreat(amnt_of_backticks + 1);
+                        break;
                     }
                 }
 
                 GREATER_THAN => {
-                    walker.retreat(1);
-                    break;
+                    // walker.retreat(1);
+                    // break;
+                    //
+                    // *walker.data().get_mut(walker.position()).unwrap() = SPACE
                 }
-
                 _ => {}
             };
         }
 
-        Block::make_paragraph(StrRange::new(initial, walker.position()), self.get_new_id())
+        Block::make_paragraph(
+            StrRange::new(initial - 1, walker.position()),
+            self.get_new_id(),
+        )
     }
 
     pub fn blockquote(&mut self, walker: &mut Walker<'_>) -> Block {
@@ -261,38 +341,81 @@ impl BlockParser {
         let level = walker.till_not(GREATER_THAN);
         let initial = walker.position();
 
+        dbg!(walker.position());
+
         while let Some(char) = walker.next() {
+            dbg!(walker.position());
             match char {
                 NEWLINE => {
                     if walker.is_next_char(NEWLINE) {
                         walker.advance(1);
                         break;
                     }
+
+                    let next = match walker.peek(0) {
+                        None => todo!(),
+                        Some(val) => val,
+                    };
+
+                    match next {
+                        NEWLINE => {
+                            walker.advance(1);
+                            break;
+                        }
+
+                        BACKTICK => {
+                            // let pos = walker.position();
+                            let amnt_of_backticks = walker.till_not(BACKTICK);
+
+                            if amnt_of_backticks < 3 {
+                                walker.retreat(amnt_of_backticks);
+                                break;
+                            }
+                        }
+
+                        HASH => {
+                            // let pos = walker.position();
+                            let amnt_of_hashes = walker.till_not(HASH);
+                            let is_after_space = walker.is_next_char(SPACE);
+
+                            if 6 > amnt_of_hashes || is_after_space {
+                                walker.retreat(amnt_of_hashes);
+                                break;
+                            }
+                        }
+
+                        _ => {}
+                    }
                 }
 
                 GREATER_THAN => {
                     let amnt_of = walker.till_not(GREATER_THAN);
 
-                    if amnt_of != level {
-                        walker.retreat(amnt_of + 1);
+                    if amnt_of > level {
+                        walker.retreat(amnt_of);
                         break;
                     }
+
+                    let advance = usize::from(walker.is_next_char(SPACE));
+                    walker.advance(advance);
                 }
 
                 _ => {}
             }
         }
 
+        dbg!(walker.position());
+
         let piece = walker
             .data()
-            .get(initial..walker.position())
+            .get(initial + 1..walker.position())
             .expect("this access should be in bounds");
 
         let mut new_walker = Walker::new(piece);
         let inner = match self.block(&mut new_walker) {
-            None => None,
+            Block::Eof => None,
 
-            Some(mut val) => {
+            mut val => {
                 val.str_range(|range| {
                     range.adjust(|(start, end)| {
                         *start += initial;
@@ -304,10 +427,11 @@ impl BlockParser {
             }
         };
 
+        dbg!(walker.position());
         Block::make_blockquote(inner, id, level)
     }
 
-    pub fn code<const CHAR: u8>(&mut self, walker: &mut Walker<'_>) -> Block {
+    pub fn fenced_code<const CHAR: u8>(&mut self, walker: &mut Walker<'_>) -> Block {
         debug_assert!(
             CHAR == TILDE || CHAR == BACKTICK,
             "invalid char provided to the `code` function"
@@ -351,6 +475,7 @@ impl BlockParser {
 
                 if amnt_of >= amnt_of_backticks {
                     code_end = walker.position() - amnt_of;
+                    walker.advance(1);
                     break;
                 }
             }
@@ -364,13 +489,49 @@ impl BlockParser {
         )
     }
 
-    pub fn heading(&mut self, walker: &mut Walker<'_>) -> Block {
+    pub fn indented_code(&mut self, walker: &mut Walker<'_>) -> Option<Block> {
+        let amnt_of_spaces = walker.till_not(SPACE);
+
+        if amnt_of_spaces < 3 {
+            walker.retreat(amnt_of_spaces);
+            return None;
+        }
+
+        let mut ranges = Vec::with_capacity(BLOCK_VEC_PREALLOCATION);
+
+        let range = walker.till_inclusive(NEWLINE);
+        ranges.push(range);
+
+        walker.advance(1);
+        self.indented_code_inner(walker, &mut ranges);
+
+        Block::make_indented_code(ranges, self.get_new_id()).into()
+    }
+
+    #[inline]
+    fn indented_code_inner(&mut self, walker: &mut Walker<'_>, accum: &mut Vec<StrRange>) {
+        let amnt_of_spaces = walker.till_not(SPACE);
+
+        if amnt_of_spaces < 4 {
+            walker.retreat(amnt_of_spaces);
+            return;
+        }
+
+        let range = walker.till_inclusive(NEWLINE);
+
+        walker.advance(1);
+        accum.push(range);
+
+        self.indented_code_inner(walker, accum)
+    }
+
+    pub fn heading(&mut self, walker: &mut Walker<'_>) -> Option<Block> {
         let level = if walker.is_next_char(HASH) {
             let temp = walker.till_not(HASH);
 
             if temp > 5 {
                 walker.retreat(temp + 1);
-                return self.paragraph(walker);
+                return None;
             };
 
             temp as u8 + 1
@@ -380,16 +541,48 @@ impl BlockParser {
 
         if !walker.is_next_char(SPACE) {
             walker.retreat(level as usize);
-            return self.paragraph(walker);
+            // dbg!(level);
+            // dbg!(walker.data().get(42));
+            //
+            let para = self.paragraph(walker);
+            dbg!(&para);
+            return para.into();
         } else {
             walker.advance(1);
         }
 
-        Block::make_heading(walker.till_inclusive(NEWLINE), level)
+        let range = walker.till_inclusive(NEWLINE);
+        walker.advance(1);
+
+        Block::make_heading(range, level, self.get_new_id()).into()
     }
 
-    pub fn special_heading(start: usize, end: usize) -> Block {
-        Block::make_heading(StrRange::new(start, end), 1)
+    #[inline]
+    pub fn special_heading(start: usize, end: usize, id: usize) -> Block {
+        Block::make_heading(StrRange::new(start, end), 1, id)
+    }
+
+    fn handle_special_heading(&mut self, walker: &mut Walker<'_>, initial: usize) -> Option<Block> {
+        let mut heading = true;
+        let pos = walker.position();
+        while let Some(char) = walker.next() {
+            match char {
+                NEWLINE => break,
+
+                EQUALS => {}
+
+                _ => {
+                    heading = false;
+                    break;
+                }
+            }
+        }
+
+        if heading {
+            Self::special_heading(initial - 1, pos - 1, self.get_new_id()).into()
+        } else {
+            None
+        }
     }
 
     pub fn style_break(&mut self, walker: &mut Walker<'_>) -> Option<Block> {
@@ -438,6 +631,57 @@ mod tests {
     use crate::{block_parser::Block, walker::Walker};
 
     #[test]
+    fn complete() {
+        let data = concat!(
+            "> Blockquote\n",
+            ">BlockquoteNoSpace\n",
+            "# Heading\n",
+            "#BrokenHeading\n",
+            "```rust,some_meta_data=noumea :3\n",
+            "panic!()\n",
+            "```\n",
+            "    Indented code!\n",
+            "--*\n",
+            "Heading with equals\n",
+            "======\n",
+            "and let's have a nice paragraph\n",
+        )
+        .as_bytes();
+        dbg!(data.len());
+
+        let mut walker = Walker::new(data);
+        let mut parser = BlockParser::new(());
+
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+
+        let block = dbg!(parser.block(&mut walker));
+        dbg!(walker.position());
+        dbg!(block.test(data));
+    }
+
+    #[test]
     fn blockquote() {
         let md = concat!(
             ">>> This is a blockquote\n",
@@ -448,9 +692,7 @@ mod tests {
         let mut parser = BlockParser::new(());
         let mut walker = Walker::new(md);
 
-        let val = parser
-            .block(&mut walker)
-            .expect("this block should be here");
+        let val = parser.block(&mut walker);
 
         let inner = match val {
             Block::Blockquote(q) => *q.text.expect("field not present"),
@@ -469,9 +711,7 @@ mod tests {
             _ => assert!(false, "block was not paragraph"),
         }
 
-        let val = parser
-            .block(&mut walker)
-            .expect("this block should be here");
+        let val = parser.block(&mut walker);
 
         let inner = match val {
             Block::Blockquote(q) => *q.text.expect("field not present"),
@@ -498,7 +738,7 @@ mod tests {
         let mut walker = Walker::new(data);
         let mut parser = BlockParser::new(());
 
-        let block = match parser.block(&mut walker).expect("expected block") {
+        let block = match parser.block(&mut walker) {
             Block::FencedCode(fc) => fc,
 
             _ => panic!("block was not fenced code"),
@@ -514,7 +754,7 @@ mod tests {
         let mut walker = Walker::new(data);
         let mut parser = BlockParser::new(());
 
-        let block = match parser.block(&mut walker).expect("expected block") {
+        let block = match parser.block(&mut walker) {
             Block::FencedCode(fc) => fc,
 
             _ => panic!("block was not fenced code"),
@@ -524,13 +764,45 @@ mod tests {
     }
 
     #[test]
+    fn code_indented() {
+        let data = concat!(
+            "       code line 1\n",
+            "       code line 2\n",
+            "       code line 3\n",
+            "       code line 4\n",
+            "       code line 5\n",
+        );
+
+        let mut walker = Walker::new(data.as_bytes());
+        let mut parser = BlockParser::new(());
+
+        let block = parser.block(&mut walker);
+
+        let inner = match block {
+            Block::IndentedCode(ic) => ic,
+            _ => panic!("block was not indented code"),
+        };
+
+        inner
+            .indents
+            .into_iter()
+            .enumerate()
+            .map(|(index, val)| (index + 1, val.resolve(data.as_bytes())))
+            .for_each(|(index, value)| {
+                let test = format!("code line {}", index);
+
+                assert!(test == value, "wrong value at line: {}", index)
+            });
+    }
+
+    #[test]
     fn heading_simple() {
         let data = "###### une, grande, et indivisible".as_bytes();
 
         let mut walker = Walker::new(data);
         let mut parser = BlockParser::new(());
 
-        let block = match parser.block(&mut walker).expect("expected block") {
+        let block = match parser.block(&mut walker) {
             Block::Heading(h) => h,
 
             _ => panic!("block was not a heading"),
@@ -555,7 +827,7 @@ mod tests {
         let mut walker = Walker::new(data);
         let mut parser = BlockParser::new(());
 
-        let block = match parser.block(&mut walker).expect("expected block") {
+        let block = match parser.block(&mut walker) {
             Block::Heading(h) => h,
 
             _ => panic!("block was not a heading"),
@@ -580,19 +852,19 @@ mod tests {
         let mut walker = Walker::new(data);
         let mut parser = BlockParser::new(());
 
-        match parser.block(&mut walker).expect("expected block") {
+        match parser.block(&mut walker) {
             Block::StyleBreak(_) => {}
 
             _ => panic!("block was not style break"),
         };
 
-        match parser.block(&mut walker).expect("expected block") {
+        match parser.block(&mut walker) {
             Block::StyleBreak(_) => {}
 
             _ => panic!("block was not style break"),
         };
 
-        match parser.block(&mut walker).expect("expected block") {
+        match parser.block(&mut walker) {
             Block::StyleBreak(_) => {}
 
             _ => panic!("block was not style break"),
