db3be82463833d6f9be865c9d84a3fee5def04b1
esther-ff <creepermaster000@gmail.com>
2025-05-15 22:30:31 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![no_std]
+
+extern crate alloc;
+
+pub(crate) mod lib {
+    pub use alloc::boxed::Box;
+    pub use alloc::string::String;
+    pub use alloc::vec::Vec;
+}
+
 mod md;
 
 pub use md::block_parser;
